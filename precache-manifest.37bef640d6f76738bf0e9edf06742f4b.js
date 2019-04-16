c137b4436967f61c8bcfd9a993ce13233c71bfb5
molnesium <hm@nabo.as>
2019-04-16 15:26:24 +0200

    initial commit

---
+++ precache-manifest.37bef640d6f76738bf0e9edf06742f4b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1226fa1174e6489869b96bcffaeb6ddb",
+    "url": "/static/media/pdf.1226fa11.png"
+  },
+  {
+    "revision": "51cfe16d7dc006ba1acf48f493d7e7cb",
+    "url": "/static/media/face2.51cfe16d.jpg"
+  },
+  {
+    "revision": "f97c97c04b9ed3674f9f03eed84fe5ce",
+    "url": "/static/media/face.f97c97c0.jpg"
+  },
+  {
+    "revision": "525c671317165f77b0d31543634093ab",
+    "url": "/static/media/calendar.525c6713.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "458ad8a7640b9a235fbc",
+    "url": "/static/js/main.536cfbe2.chunk.js"
+  },
+  {
+    "revision": "23a01bcd817b6a8d81e2",
+    "url": "/static/js/2.c2fcb507.chunk.js"
+  },
+  {
+    "revision": "458ad8a7640b9a235fbc",
+    "url": "/static/css/main.06292dda.chunk.css"
+  },
+  {
+    "revision": "d665e085df81c85d3fd6770e6b716d40",
+    "url": "/index.html"
+  }
+];
