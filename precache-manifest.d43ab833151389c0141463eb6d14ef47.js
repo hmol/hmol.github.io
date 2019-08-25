f120fa90baacefe182a192b69f8f790cb4ff4652
molnesium <hm@nabo.as>
2019-08-25 09:31:52 +0200

    new build of static content

---
+++ precache-manifest.d43ab833151389c0141463eb6d14ef47.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1226fa1174e6489869b96bcffaeb6ddb",
+    "url": "/static/media/pdf.1226fa11.png"
+  },
+  {
+    "revision": "fd87cb6b568d800fde6b",
+    "url": "/static/css/main.5a49f0d3.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8e49a08f38e825b10668",
+    "url": "/static/js/2.13130c5a.chunk.js"
+  },
+  {
+    "revision": "51cfe16d7dc006ba1acf48f493d7e7cb",
+    "url": "/static/media/face2.51cfe16d.jpg"
+  },
+  {
+    "revision": "6b83dde89a8ccbdeff70a97c17303739",
+    "url": "/static/media/face_terminator.6b83dde8.jpg"
+  },
+  {
+    "revision": "525c671317165f77b0d31543634093ab",
+    "url": "/static/media/calendar.525c6713.svg"
+  },
+  {
+    "revision": "fd87cb6b568d800fde6b",
+    "url": "/static/js/main.5a409918.chunk.js"
+  },
+  {
+    "revision": "bdf0f9e858b5431e9cf0566250c4fdc0",
+    "url": "/static/media/tone.bdf0f9e8.svg"
+  },
+  {
+    "revision": "8057d431a851a02a19197474941ff4a3",
+    "url": "/static/media/C64_Pro_Mono-STYLE.8057d431.woff"
+  },
+  {
+    "revision": "2712b79ca831a1a93a242c59c3c01d61",
+    "url": "/static/media/C64_Pro_Mono-STYLE.2712b79c.woff2"
+  },
+  {
+    "revision": "974002210360fe83c16eb44842d6ad86",
+    "url": "/static/media/river.97400221.jpg"
+  },
+  {
+    "revision": "eddf618f6c14e2ffff26e785bd56b2e4",
+    "url": "/static/media/nightsky.eddf618f.jpg"
+  },
+  {
+    "revision": "6bc5e9294ed46d350b5fc57e5dc716d9",
+    "url": "/index.html"
+  }
+];
