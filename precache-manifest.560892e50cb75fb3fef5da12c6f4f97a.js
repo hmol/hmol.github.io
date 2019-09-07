a81ff238d12948f8cf55f10e1d628b41420d901d
molnesium <hm@nabo.as>
2019-09-07 22:53:48 +0200

    new build of static content

---
+++ precache-manifest.560892e50cb75fb3fef5da12c6f4f97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048a356835bfd4c2190c0054173d1548",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d96120310a0c2ccd5ca1",
+    "url": "/static/css/main.e3826d86.chunk.css"
+  },
+  {
+    "revision": "46ecf5d1647bc9d925b1",
+    "url": "/static/js/2.1b125461.chunk.js"
+  },
+  {
+    "revision": "d96120310a0c2ccd5ca1",
+    "url": "/static/js/main.27a80297.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  },
+  {
+    "revision": "525c671317165f77b0d31543634093ab",
+    "url": "/static/media/calendar.525c6713.svg"
+  }
+]);
