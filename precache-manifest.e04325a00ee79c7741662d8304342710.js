9bda5029debae2fd9a649e8d184f2a1075314269
hmol <molnes@gmail.com>
2020-12-03 12:56:21 +0100

    new build of static content

---
+++ precache-manifest.e04325a00ee79c7741662d8304342710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1eb5f4959cdda92d51de47119a1df35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08c77333404862e2e43b",
+    "url": "/static/css/main.f2206f71.chunk.css"
+  },
+  {
+    "revision": "51d7a877392e8beb6dd3",
+    "url": "/static/js/2.5d65bece.chunk.js"
+  },
+  {
+    "revision": "08c77333404862e2e43b",
+    "url": "/static/js/main.3a4e5810.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  },
+  {
+    "revision": "525c671317165f77b0d31543634093ab",
+    "url": "/static/media/calendar.525c6713.svg"
+  },
+  {
+    "revision": "28c049166b23ce4cdafeab873bf88f44",
+    "url": "/static/media/face.28c04916.jpg"
+  }
+]);
